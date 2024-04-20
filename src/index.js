bda3703f9de309c56ca0eabac62cf0d98346fc0b
moon <410518072@qq.com>
2024-04-21 01:18:58 +0800

    init

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ var version = "1.0.0"
 
 console.log('hutool v' + version)
 
-
-export * from "./common"
-export * from "./date"
+import * as hutool from "./index.inc"
+export * from "./index.inc"
+export default hutool
 
