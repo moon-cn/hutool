8419562d4e53e2c286271801ec1967bbbd08149b
jiangtao <410518072@qq.com>
2024-05-04 13:14:26 +0800

    test

--- src/url.js
+++ src/url.js
@@ -1,22 +1,23 @@
-// 浏览器相关 - url
-
 /**
  * 获取url的参数
- * @param url
+ * @param urlStr
  * @returns {*|{}|{}}
  */
-export function params(url) {
-    url = url || window.location.search;
-    if (!url.contains('?')) {
-        return {};
+export function params(urlStr) {
+    if (!urlStr) {
+        return {}
     }
 
-    url = url.substringAfter('?')
+    var url = new URL(urlStr);
+    var params = new URLSearchParams(url.search);
 
-    // TODO , 手动实现
-    let params = qs.parse(url);
+    const result = {}
+    for (const [key, value] of params.entries()) {
+        console.log(key + ': ' + value);
+        result[key] = value;
+    }
 
-    return params || {};
+    return result
 
 }
 
@@ -25,32 +26,38 @@ export function params(url) {
  * @param url
  */
 export function baseUrl(url) {
+    if (!url) {
+        return null
+    }
     return new URL(url).origin
 }
 
-export function removeParams(url) {
-    url = url || window.location.search;
-    if (url.contains('?')) {
-        return url.substringBefore('?')
+
+/**
+ * 将参数对象转换为 url中的
+ * @param params
+ */
+export function paramsToSearch(params) {
+    if (!params) {
+        return "";
     }
-    return url;
+
+    const buffer = []
+
+    for (let k in params) {
+        let v = params[k]
+
+        buffer.push(k + '=' + v);
+
+    }
+
+    return buffer.join('&')
 
 }
 
 export function replaceParam(url, key, value) {
-    const params = this.params(url)
-    params[key] = value;
+    const p = params(url)
+    p[key] = value;
 
-    const search = [];
-    for (let k in params) {
-        let v = params[k];
-        if (v) {
-            search.push(k + "=" + v);
-        }
-    }
-
-
-    url = removeParams(url)
-    url += '?' + search.join("&");
-    return url;
+    return baseUrl(url) + '?' + paramsToSearch(p);
 }
