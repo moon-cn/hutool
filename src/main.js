d646cb093d3df018fedc15478f490da1126dd5d8
jiangtao <410518072@qq.com>
2024-07-14 17:35:48 +0800

    更换打包工具

--- src/main.js
+++ src/main.js
@@ -1,15 +1,25 @@
-export * as storage from "./storage";
-export * as date from "./date";
-export * as http from "./http";
-export * as arr from "./arr";
-export * as html from "./html";
-export * as tree from "./tree";
-export * as url from "./url"
-export * as str from "./str"
-export {eventBus} from "./eventBus";
-export * as color from "./color"
-export {debounce} from "./debounce";
-export * as dom from "./dom"
-export {uid} from "./uid";
-export * as validate from  './validate'
+import * as storage from "./storage";
+import * as date from "./date";
+import * as http from "./http";
+import * as arr from "./arr";
+import * as html from "./html";
+import * as tree from "./tree";
+import * as url from "./url"
+import * as str from "./str"
+import {eventBus} from "./eventBus";
+import * as color from "./color"
+import {debounce} from "./debounce";
+import * as dom from "./dom"
+import {uid} from "./uid";
+import * as validate from './validate'
 
+
+const hutool = {
+    storage, date, http, arr, html, tree, url, str, eventBus, color, debounce, dom, uid, validate
+}
+export default hutool
+
+
+export {
+    storage, date, http, arr, html, tree, url, str, eventBus, color, debounce, dom, uid, validate
+}
